638a50a5dcf405eb7f52b52e45e0ce2af0633831
GuilhermeACSM <zguilhermegn@gmail.com>
2024-09-03 09:49:34 -0300

    Atualizado

--- Exs-Js_Pratica/Ex-1/script.js
+++ Exs-Js_Pratica/Ex-1/script.js
@@ -5,6 +5,7 @@ let p = window.document.getElementById('parag')
 b.addEventListener('click', clicar)
 
 function clicar() {
+    //Usar 'value' depois da variavel pois ele quer somente o valor da div/input
     if(a.value > 60) {
         p.innerText = 'Passou do limite de velocidade!'
     }
