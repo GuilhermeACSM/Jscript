dc22ae18506e7758a54795694af85d06c4bc96a6
GuilhermeACSM <zguilhermegn@gmail.com>
2024-09-05 11:40:09 -0300

    Atualizado

---
+++ Ex-Idade/script.js
@@ -0,0 +1,14 @@
+
+
+function clicar() {
+    let ano = document.getElementById('ano')
+
+
+
+    if (ano < 1900 && ano > 2023) {
+        alert('ERRO - Digite uma data válida (1900 à 2023)')
+    }
+}
+
+
+
