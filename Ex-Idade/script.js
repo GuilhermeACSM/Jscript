d34bc46642290270e73a53642dbf5aaf707f0475
Guilherme Augusto <zguilhermegn@gmail.com>
2024-09-05 21:09:54 -0300

    atualizar

--- Ex-Idade/script.js
+++ Ex-Idade/script.js
@@ -1,13 +1,11 @@
-
+let b = document.getElementById('verify')
+b.addEventListener('click', clicar)
 
 function clicar() {
     let ano = document.getElementById('ano')
 
 
-
-    if (ano < 1900 && ano > 2023) {
-        alert('ERRO - Digite uma data válida (1900 à 2023)')
-    }
+    
 }
 
 
