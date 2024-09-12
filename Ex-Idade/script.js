038d50b1f49f219f02e19eac05150d730bf65f2b
GuilhermeACSM <zguilhermegn@gmail.com>
2024-09-12 08:33:07 -0300

    Atualizado

--- Ex-Idade/script.js
+++ Ex-Idade/script.js
@@ -24,27 +24,38 @@ function clicar() {
     switch (genero) {
         case 'M':
             if (idade > 124 || idade < 1) {
-            alert('[ERRO] - Digite uma data válida (De 1900 à 2023)')
+                alert('[ERRO] - Digite uma data válida (De 1900 à 2023)')
+                img.style.display = 'none'
             }
             else if (idade <= 2) {
-            parag.innerText = `Você é um bebê de ${idade} anos!`
-            img.src = 'bebemenino.png'
+                parag.innerText = `Você é um bebê de ${idade} anos!`
+                img.src = 'bebemenino.png'
+                img.style.display = 'block'
+                img.style.margin = 'auto'
             }
             else if (idade <= 14) {
-            parag.innerText = `Você é uma criança de ${idade} anos!`
-            img.src = 'criancamenino.webp'
+                parag.innerText = `Você é uma criança de ${idade} anos!`
+                img.src = 'criancamenino.webp'
+                img.style.display = 'block'
+                img.style.margin = 'auto'
             }
             else if (idade <= 18) {
-            parag.innerText = `Você é um adolescente de ${idade} anos!`
-            img.src = 'adolescentemasc.jpg'
+                parag.innerText = `Você é um adolescente de ${idade} anos!`
+                img.src = 'adolescentemasc.jpg'
+                img.style.display = 'block'
+                img.style.margin = 'auto'
             }
             else if (idade <= 60) {
-            parag.innerText = `Você é um homem de ${idade} anos!`
-            img.src = 'homem.jpg'
+                parag.innerText = `Você é um homem de ${idade} anos!`
+                img.src = 'homem.jpg'
+                img.style.display = 'block'
+                img.style.margin = 'auto'
             }
             else {
-            parag.innerText = `Você é um senhor de ${idade} anos!`
-            img.src = 'idoso.jpg'
+                parag.innerText = `Você é um senhor de ${idade} anos!`
+                img.src = 'idoso.jpg'
+                img.style.display = 'block'
+                img.style.margin = 'auto'
             }
             break 
 
@@ -57,22 +68,32 @@ function clicar() {
             else if (idade <= 2) {
                 parag.innerText = `Você é uma bebê de ${idade} anos!`
                 img.src = 'bebemenina.jpg'
+                img.style.display = 'block'
+                img.style.margin = 'auto'
             }
             else if (idade <= 14) {
                 parag.innerText = `Você é uma criança de ${idade} anos!`
                 img.src = 'criancamenina.jpg'
+                img.style.display = 'block'
+                img.style.margin = 'auto'
             }
             else if (idade <= 18) {
                 parag.innerText = `Você é uma adolescente de ${idade} anos!`
                 img.src = 'adolescentefem.jpg'
+                img.style.display = 'block'
+                img.style.margin = 'auto'
             }
             else if (idade <= 60) {
                 parag.innerText = `Você é uma mulher de ${idade} anos!`
                 img.src = 'mulher.jpg'
+                img.style.display = 'block'
+                img.style.margin = 'auto'
             }
             else {
                 parag.innerText = `Você é uma senhora de ${idade} anos!`
                 img.src = 'idosa.jpeg'
+                img.style.display = 'block'
+                img.style.margin = 'auto'
             }
             break
     }
