5b30b8b55a2616840680cdb9f72638fb0986f318
Guilherme Augusto <zguilhermegn@gmail.com>
2024-09-17 19:36:58 -0300

    Atualizado

--- Ex-Vamos_Contar/script.js
+++ Ex-Vamos_Contar/script.js
@@ -2,26 +2,32 @@ function clicar() {
     let inicio = document.getElementById('inicio')
     let fim = document.getElementById('fim')
     let passos = document.getElementById('passos')
-
-    let start = Number(inicio.value)
-    let end = Number(fim.value)
-    let lag = Number(passos.value)
-
-    //variavel de paragrafo final do contador
     let parag = document.getElementById('parag')
 
-    //string para acumular os números
-    let sfim = ''
-    
-    for(start; start <= end; start += lag) {
-        //sfim =  sfim + `${start} ⭐`  Mesma forma de fazer!
-        sfim += `${start} ⭐` //Fica mais bonito e enxuto!
+    let i = Number(inicio.value)
+    let f = Number(fim.value)
+    let p = Number(passos.value)
+
+    if (inicio.value.lenght == 0 || fim.value.lenght == 0 || passos.value.lenght == 0) {
+        window.alert('[ERRO] Faltam Dados!')
+    } else {
+        parag.innerHTML = 'Contando <br>'
+        if (p <= 0) {
+            // Se colocar (passo = 0) ele ira contar de 1 em 1 (1 - 2 - 3 - 4...)!!
+            alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+        // Contagem crescente
+        if (i < f) {
+            for(let c = i ; c <= f ; c += p) {
+                parag.innerHTML += `${c} \u{1F449}`
+            }
+        } else {
+            // Contagem regressiva
+            for(let c = i ; c >= f ; c -= p) {
+                parag.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        parag.innerHTML += `\u{1F3C1}`
     }
-
-    //adiciona bandeira no final
-    //emoji \uD83C\uDFC1
-    sfim += '🏁'
-
-    parag.style.textAlign = "left"
-    parag.innerText = `${sfim}`
 }
