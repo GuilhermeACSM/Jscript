7e0b34907e28e27d97e73c1efbc784c4170e6727
Guilherme Augusto <zguilhermegn@gmail.com>
2024-09-17 20:37:12 -0300

    Atualizado

--- Ex-Vamos_Contar/script.js
+++ Ex-Vamos_Contar/script.js
@@ -4,14 +4,13 @@ function clicar() {
     let passos = document.getElementById('passos')
     let parag = document.getElementById('parag')
 
-    let i = Number(inicio.value)
-    let f = Number(fim.value)
-    let p = Number(passos.value)
-
     if (inicio.value.lenght == 0 || fim.value.lenght == 0 || passos.value.lenght == 0) {
         window.alert('[ERRO] Faltam Dados!')
     } else {
         parag.innerHTML = 'Contando: <br>'
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passos.value)
         if (p <= 0) {
             // Se colocar (passo = 0) ele ira contar de 1 em 1 (1 - 2 - 3 - 4...)!!
             alert('Passo inválido! Considerando PASSO 1')
