c2e01f2cd49818364d7e577a6e1b24059c5e688c
GuilhermeACSM <zguilhermegn@gmail.com>
2024-09-23 08:22:00 -0300

    Atualizado

--- Ex-Números_Primos/script.js
+++ Ex-Números_Primos/script.js
@@ -2,19 +2,19 @@ function clicar() {
     let textn = document.getElementById('number')
     let texta = document.getElementById('texta')
 
-    let num = Number(textn.value)
 
     if(textn.value == 0) {
         alert('[ERRO] Digite um número')
+        
+        texta.innerHTML = ' '
+        for(let i = 1; i <= 10 ; i++) {
+            
+            texta.innerHTML = `${i}`
+        }
     }
+    let num = Number(textn.value)
     
-    texta.innerHTML = ""
-    if(num <= 1) return false
-    for(let i = 2 ; i <= Math.sqrt(num) ; i++) {
-        if(num % i === 0) return false
-    }
-    return true
-
+    
     
 
 }
