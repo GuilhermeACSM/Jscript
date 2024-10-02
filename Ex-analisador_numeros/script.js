48c6400b6d419641bc14936af7b589aa84886b94
GuilhermeACSM <zguilhermegn@gmail.com>
2024-10-02 08:52:08 -0300

    Atualizado

--- Ex-analisador_numeros/script.js
+++ Ex-analisador_numeros/script.js
@@ -3,6 +3,22 @@ let lista = document.getElementById('lista')
 let res =  document.getElementById('resultado')
 let valores = []
 
+// Função para adicionar o número no array!!!
+function adicionar() {
+    if (isNumero(num.value) && !inLista(num.value, valores)) {
+        valores.push(Number(num.value))
+        let option = document.createElement("option")
+        option.innerHTML = `Valor ${num.value} adicionado`
+        lista.appendChild(option)
+        console.log(valores)
+        res.innerHTML = ""
+    } else {
+        alert('Número encontrado na lista ou inválido!')
+    }
+    num.value = ""
+    num.focus() 
+}
+
 function isNumero(n) {
     if (Number(n) >= 1 && Number(n) <= 100) {
         return true
@@ -19,21 +35,36 @@ function inLista(n, l) {
     }
 }
 
-function adicionar() {
-    if (isNumero(num.value) && !inLista(num.value, valores)) {
-        valores.push(Number(num.value))
-        let option = document.createElement("option")
-        option.innerHTML = `Valor ${num.value} adicionado`
-        lista.appendChild(option)
-        console.log(valores)
-        res.innerHTML = ""
+// Função para finalizar e fazer as contas com os números do array!!!
+function finalizar() {
+    let total = valores.length
+    let soma = 0
+    let media = 0
+
+    if (valores.length <= 0) {
+        window.alert('Insira valores na lista antes de calcular!')
+
     } else {
-        alert('Número encontrado na lista ou inválido!')
+        for (let pos in valores) {
+            soma += Number(valores[pos])
+        }
+        media = soma / valores.length
+        Math.round(media, 2)
+
+        valores.sort((a, b) => a - b)
+
+        resultado.innerHTML =  `<p>Ao todo, temos <strong>${total}</strong> números cadastrados!</p> 
+                                <p>O menor valor da lista é <strong>${Number(valores[0])}</strong></p>
+                                <p>O maior valor da lista é <strong>${Number(valores[total - 1])}</strong></p>
+                                <p>Soma dos valores do Vetor: <strong>${soma}</strong></p>
+                                <p>A média dos valores da lista é: <strong>${media}</strong>`
+        num.focus()
     }
-    num.value = ""
-    num.focus() 
+    num.focus()
 }
 
+// Jeito de finalzar com biblioteca!!
+/*
 function finalizar() {
     if( valores.length > 0) {
         let soma = valores.reduce((a, b) => a + b, 0)
@@ -53,3 +84,4 @@ function finalizar() {
         alert('Adicione um número antes de finalizar!')
     }
 }
+*/
