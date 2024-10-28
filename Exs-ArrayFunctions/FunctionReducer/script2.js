0fce0753eff80f8e3c0bdce1c0fb010e82e6d600
GuilhermeACSM <zguilhermegn@gmail.com>
2024-10-28 10:01:26 -0300

    Atualizado

---
+++ Exs-ArrayFunctions/FunctionReducer/script2.js
@@ -0,0 +1,7 @@
+const transacoes = [ 
+    { tipo: 'deposito', valor: 100 }, 
+    { tipo: 'retirada', valor: 50 }, 
+    { tipo: 'deposito', valor: 200 } 
+];
+
+const saldoFinal = transacoes.reduce()
