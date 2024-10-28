5bbd5a05d2e8e927d458b3592b6bd29b7a3bfc2c
GuilhermeACSM <zguilhermegn@gmail.com>
2024-10-28 11:04:39 -0300

    Atualizado

--- Exs-ArrayFunctions/FunctionReducer/script2.js
+++ Exs-ArrayFunctions/FunctionReducer/script2.js
@@ -4,4 +4,12 @@ const transacoes = [
     { tipo: 'deposito', valor: 200 } 
 ];
 
-const saldoFinal = transacoes.reduce()
+const saldoFinal = transacoes.reduce((saldo, transacao) => {
+    if (transacao.tipo === 'deposito') {
+        return saldo + transacao.valor;
+    } else if (transacao.tipo === 'retirada') {
+        return saldo - transacao.valor;
+    }   
+}, 0);
+
+console.log(saldoFinal);
