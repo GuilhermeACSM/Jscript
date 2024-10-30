ef561ad19bb95992b46805fdf12c3e2897049683
GuilhermeACSM <zguilhermegn@gmail.com>
2024-10-30 11:37:54 -0300

    Atualizado

--- Exs-testes/Ex-Lista_de_tarefas/script.js
+++ Exs-testes/Ex-Lista_de_tarefas/script.js
@@ -1,14 +1,1 @@
-function clicar() {
-    let text = document.getElementById('text')
-    let parag = document.getElementById('parag')
 
-    let txt = text.value
-
-    for(text ; text <= txt; text++) {
-        parag.innerText += ` - ${txt} \n`
-    }
-}
-function excluir() {
-    let parag = document.getElementById('parag');
-    parag.innerText = ''; // Limpa todo o conteúdo do parágrafo
-}
